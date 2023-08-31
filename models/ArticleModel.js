0478bca77eb2c35531162484d79c4f5c805ed7a1
alexthedev225 <alexcode225@gmail.com>
2023-08-31 13:25:20 +0000

    phase de développement

--- models/ArticleModel.js
+++ models/ArticleModel.js
@@ -5,7 +5,9 @@ const articleSchema = new mongoose.Schema({
   content: { type: String, required: true },
   image: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true } // Référence à l'utilisateur (auteur)
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }, // Référence à la catégorie de l'article
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }], // Références aux commentaires liés à l'article
 });
 
 const Article = mongoose.model("Article", articleSchema);
--- models/CommentModel.js
+++ models/CommentModel.js
@@ -1,15 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const articleSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+const commentSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  image: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
   createdAt: { type: Date, default: Date.now },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }, // Référence à la catégorie de l'article
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }], // Références aux commentaires liés à l'article
 });
 
-const Article = mongoose.model("Article", articleSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
-module.exports = Article;
+module.exports = Comment;
