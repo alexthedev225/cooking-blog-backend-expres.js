3421b2761ed0e833f468b12b88b59d79fff6a0e6
alexthedev225 <alexcode225@gmail.com>
2023-09-06 17:28:34 +0000

    phase de laboratoire v1

--- models/ArticleModel.js
+++ models/ArticleModel.js
@@ -8,7 +8,7 @@ const articleSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   // category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }, // Référence à la catégorie de l'article
-  // comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }], // Références aux commentaires liés à l'article
+  comments: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" }, // Références aux commentaires liés à l'article
 });
 
 const Article = mongoose.model("Article", articleSchema);
