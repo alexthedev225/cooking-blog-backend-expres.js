616c46c348d3bddf73b0fc51233b9279216a2d89
alexthedev225 <alexcode225@gmail.com>
2023-09-09 03:43:20 +0000

    socket.io ajouté

--- socketHandler.js
+++ socketHandler.js
@@ -1,5 +1,4 @@
 const socketIo = require("socket.io");
-const Comment = require('./models/CommentModel');
 
 let io; // Initialisez io en tant que variable globale pour pouvoir y accéder ailleurs
 
@@ -14,22 +13,8 @@ module.exports = {
 
     io.on("connection", (socket) => {
       console.log("Client connecté");
-
       socket.on("disconnect", () => {
         console.log("Client déconnecté");
-      });
-
-      // Écoutez l'événement "get_comments_article" pour récupérer les commentaires d'un article spécifique
-      socket.on("get_comments_article", async (articleId) => {
-        try {
-          // Récupérez les commentaires spécifiques à l'article depuis la base de données
-          const comments = await Comment.find({ article: articleId });
-
-          // Émettez les commentaires au client
-          io.to(socket.id).emit(`comments_article_${articleId}`, comments);
-        } catch (error) {
-          console.error("Erreur lors de la récupération des commentaires de l'article :", error);
-        }
       });
     });
   },
