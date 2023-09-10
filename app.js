f42262f1739f82a0141310cb26070b5ab415c53f
alexthedev225 <alexcode225@gmail.com>
2023-09-10 17:44:37 +0000

    Modification de la configuration de cors

--- app.js
+++ app.js
@@ -18,15 +18,7 @@ app.use(
     credentials: true, // Autorisez les cookies, si nécessaire
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "https://cooking-fever.vercel.app",
-    "http://localhost:3000",
-  ]);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 require("dotenv").config();
 
 const PORT = process.env.PORT;
