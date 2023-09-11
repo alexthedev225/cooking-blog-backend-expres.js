d13d3964b010ce4a360e55a04254c87863d708be
alexthedev225 <alexcode225@gmail.com>
2023-09-11 17:49:53 +0000

    Modification de la configuration de socket.io

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -7,44 +7,46 @@ const commentController = {
   createComment: async (req, res) => {
     try {
       const { content } = req.body;
+      const createdAt = new Date(); // Initialisez la date de création ici
       const author = req.auth.userId;
       const articleId = req.params.id;
-  
+
       // Obtenez les informations de l'auteur à partir de la base de données
       const authorData = await User.findById(author);
-  
+
       if (!authorData) {
         socketHandler.getIO().emit("comment_error", "Auteur non trouvé");
         return res.status(404).json({ message: "Auteur non trouvé" });
       }
-  
+
       const newComment = new Comment({
         content,
         author,
         authorName: authorData.name,
         authorImage: authorData.imageProfil,
         article: articleId,
+        createdAt, // Utilisez la date de création ici
       });
-  
+
       const article = await Article.findById(articleId);
-  
+
       if (!article) {
         socketHandler.getIO().emit("comment_error", "Article non trouvé");
         return res.status(404).json({ message: "Article non trouvé" });
       }
-  
+
       article.comments.push(newComment);
-  
+
       await Promise.all([newComment.save(), article.save()]);
-  
+
       // Émettez le nouveau commentaire avec la date de création actuelle
       socketHandler.getIO().emit(`comments_article_${articleId}`, {
         comment: newComment,
-        createdAt: new Date(), // Date de création actuelle
+        createdAt, // Date de création actuelle
       });
-  
+
       console.log("Nouveau commentaire émis :", newComment);
-  
+
       res.json(newComment);
     } catch (error) {
       console.error("Erreur lors de la création du commentaire", error);
--- models/CommentModel.js
+++ models/CommentModel.js
@@ -4,7 +4,7 @@ const commentSchema = new mongoose.Schema({
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date },
   authorName: { type: String }, // Ajoutez le nom de l'auteur au commentaire
   authorImage: { type: Buffer }, // Ajoutez l'image de l'auteur au commentaire
 });
